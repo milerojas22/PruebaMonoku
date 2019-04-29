f79011b95eb4de606ba0a71efea06f66db53b1ff
milerojas22 <1203mile2205@gmail.com>
2019-04-29 16:48:26 -0500

    agregar html con vistas.

--- monoku/views.py
+++ monoku/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import *
-
+from .models import Participante
+from .models import Producto
 # Create your views here.
-def post_list(request):
-    return render(request, 'monoku/post_list.html', {})
+def participante_lista(request):
+    participantes = Participante.objects.all()
+    productos = Producto.objects.all()
+    return render(request, 'monoku/post_list.html', {'participantes':participantes, 'productos':productos})
+
